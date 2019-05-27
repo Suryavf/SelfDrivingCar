c481010e5988ae34b0f9068a29101420de60f64b
Victor <suryavf@gmail.com>
2019-05-26 21:16:06 -0500

    Fixed batch generator bug

--- ImitationLearning/BatchGenerator.py
+++ ImitationLearning/BatchGenerator.py
@@ -33,6 +33,20 @@ def CoRL2017(path):
     n_filesGroup = len(fileList)
     n_groups     = np.floor(n_filesGroup/config.filesPerGroup) - 1
 
+    # Data Augmentation
+    st = lambda aug: iaa.Sometimes(0.40, aug)
+    oc = lambda aug: iaa.Sometimes(0.30, aug)
+    rl = lambda aug: iaa.Sometimes(0.09, aug)
+
+    seq = iaa.Sequential([rl(iaa.GaussianBlur((0, 1.5))),                                               # blur images with a sigma between 0 and 1.5
+                             rl(iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05), per_channel=0.5)),     # add gaussian noise to images
+                             oc(iaa.Dropout((0.0, 0.10), per_channel=0.5)),                                # randomly remove up to X% of the pixels
+                             oc(iaa.CoarseDropout((0.0, 0.10), size_percent=(0.08, 0.2),per_channel=0.5)), # randomly remove up to X% of the pixels
+                             oc(iaa.Add((-40, 40), per_channel=0.5)),                                      # adjust brightness of images (-X to Y% of original value)
+                             st(iaa.Multiply((0.10, 2.5), per_channel=0.2)),                               # adjust brightness of images (X -Y % of original value)
+                             rl(iaa.ContrastNormalization((0.5, 1.5), per_channel=0.5)),                   # adjust the contrast
+                         ],random_order=True)
+
     while True:
         # Groups
         for n in range(n_groups):
@@ -44,8 +58,12 @@ def CoRL2017(path):
                 frames = file.frame()
                 meta   = file.getDataFrames()
 
-                for frame,info in zip(frames,meta):
+                z = list(zip(frames,meta))
+                random.shuffle(z)
+
+                for frame,info in z:
                     selectBranch = commandCode(info["Command"])
+                    frame        = seq.augment_image(frame)
 
                     yield ({'input_1': frame, 
                             'input_2': info["Speed"], 
@@ -57,5 +75,4 @@ def CoRL2017(path):
                                                   info["Gas"],
                                                   info["Brake"],
                                                   info["Speed"]] )})
-                
-
+                file.close()
