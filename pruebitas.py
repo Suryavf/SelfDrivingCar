6aa163c101c9367d77b04d3e2f21ec68284d2d26
Victor Flores <suryavf@gmail.com>
2019-06-04 14:53:05 -0500

    File read test

--- pruebitas.py
+++ pruebitas.py
@@ -560,90 +560,18 @@ imgStack = Dropout(0.2)(imgStack)
 #trainPath = "/media/victor/Datos/CORL2017ImitationLearningData/AgentHuman/SeqTrain"
 #TrainGenerator = BatchGenerator(trainPath)
 
-import numpy as np
-import keras
 
-class DataGenerator(keras.utils.Sequence):
-    'Generates data for Keras'
-    def __init__(self, list_IDs, labels, batch_size=32, dim=(32,32,32), n_channels=1,
-                 n_classes=10, shuffle=True):
-        'Initialization'
-        self.dim = dim
-        self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.shuffle = shuffle
-        self.on_epoch_end()
+path = "/media/victor/Datos/CORL2017ImitationLearningData/AgentHuman/SeqTrain"
+fileList = [path + "/" + f for f in listdir(path) if isfile(join(path, f))]
 
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
+for p in fileList:
+    # Data
+    print("File:",p)
+    file = h5py.File(p, 'r')
+    print("Read check")
 
-    def __getitem__(self, index):
-        'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
-        # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
-
-        return X, y
-
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
-        # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
-
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            # Store sample
-            X[i,] = np.load('data/' + ID + '.npy')
-
-            # Store class
-            y[i] = self.labels[ID]
-
-        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
-    
-    
-import numpy as np
-
-from keras.models import Sequential
-from my_classes import DataGenerator
-
-# Parameters
-params = {'dim': (32,32,32),
-          'batch_size': 64,
-          'n_classes': 6,
-          'n_channels': 1,
-          'shuffle': True}
-
-# Datasets
-partition = # IDs
-labels = # Labels
-
-# Generators
-training_generator = DataGenerator(partition['train'], labels, **params)
-validation_generator = DataGenerator(partition['validation'], labels, **params)
-
-# Design model
-model = Sequential()
-[...] # Architecture
-model.compile()
-
-# Train model on dataset
-model.fit_generator(generator=training_generator,
-                    validation_data=validation_generator,
-                    use_multiprocessing=True,
-                    workers=6)
+    print("RGB:",    file['rgb'].value.shape)
+    print("RGB check")
+    print("Targets:",file['targets'].value.shape)
+    print("Targets check")
+    print("\n")
