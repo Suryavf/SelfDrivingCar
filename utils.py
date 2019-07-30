a608693cdcdbaf64243a3be827db9985d13e38da
Victor Flores <suryavf@gmail.com>
2019-07-29 19:31:06 -0500

    Delete utils.py obsolete

--- utils.py
+++
@@ -1,8 +0,0 @@
-
-class Setting(object):
-    datapath =    "./data" 
-    netpath  =    "./nets"
-    host     = "localhost"
-    port     =       2000
-    city     =    "Town01"
-    model    =  "endToEnd"
