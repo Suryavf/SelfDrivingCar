65471f3e4cc816b680bd81a7bb970f8178c2265e
Victor Flores <suryavf@gmail.com>
2019-08-30 23:59:30 -0500

    Update settings. Add save and print functions.

--- config.py
+++ config.py
@@ -48,42 +48,17 @@ _outputSpeedList     = ['Codevilla19']
 
 class BooleanConditions(object):
     def __init__(self,model ):
-        self.branches        = False
-        self.multimodal      = False
-        self.inputSpeed      = False
-        self.outputSpeed     = False
-        self.speedRegression = False
+        self.branches        = False    # Conditional (branches)
+        self.multimodal      = False    # Multimodal (image + speed)
+        self.inputSpeed      = False    # Input speed
+        self.outputSpeed     = False    # Output speed
+        self.speedRegression = False    # Speed regression
         
-        # Conditional (branches)
-        if model in _branchesList:
-            self.branches = True
-        else:
-            self.branches = False
-
-        # Multimodal (image + speed)
-        if model in _multimodalList:
-            self.multimodal = True
-        else:
-            self.multimodal = False
-
-        # Speed regression
-        if model in _speedRegressionList:
-            self.speedRegression = True
-        else:
-            self.speedRegression = False
-        
-        # Input speed
-        if model in _inputSpeedList:
-            self.inputSpeed = True
-        else:
-            self.inputSpeed = False
-
-        # Output speed
-        if model in _outputSpeedList:
-            self.outputSpeed = True
-        else:
-            self.outputSpeed = False
-
+        self.branches        = model in _branchesList
+        self.multimodal      = model in _multimodalList
+        self.inputSpeed      = model in _inputSpeedList
+        self.outputSpeed     = model in _outputSpeedList
+        self.speedRegression = model in _speedRegressionList
 
 
 class Init(object):
@@ -103,12 +78,27 @@ class Init(object):
         else:
             self.seed = seed
 
-        torch.manual_seed(self.seed)
-        np.random   .seed(self.seed)
+        torch.cuda.manual_seed_all(self.seed)
+        torch.     manual_seed    (self.seed)
+        np.random        .seed    (self.seed)
 
     def device_(self,type_):
         self.device_name = type_
-        self.device = torch.device(type_)
+        self.device      = torch.device(type_)
+
+    def load(self,path):
+        self.manual_seed = True
+        with open(path) as json_file:
+            data = json.load(json_file)
+            # Multitask
+            self.num_workers = data["num_workers"]
+            # Device
+            self.device_(data["device_name"])
+            # Seed
+            self.set_seed(data["seed"])
+
+    def print(self):
+        pass
 
     def save(self,path):
         setting = {
@@ -132,6 +122,21 @@ class General_settings(object):
         self.validPath = "./data/h5file/SeqVal/"
         self.trainPath = "./data/h5file/SeqTrain/"
         self.savedPath = "/media/victor/Datos/Saved/"
+
+    def load(self,data):
+        self.stepView       = data[      "stepView"]
+        self.framePerFile   = data[  "framePerFile"]
+        self.framePerSecond = data["framePerSecond"]
+        
+        self.validPath = data["validPath"]
+        self.trainPath = data["trainPath"]
+        self.savedPath = data["savedPath"]
+
+    def print(self):
+        print("\t"*2,"Train Path:\t\t"   ,self.trainPath)
+        print("\t"*2,"Validation Path:\t",self.validPath)
+        print("\t"*2,"Saved Path:\t\t"   ,self.savedPath)
+        print("")
 
     def save(self):
         return {
@@ -157,6 +162,26 @@ class Preprocessing_settings(object):
         self.max_speed    =  90
         self.max_steering = 1.2
 
+    def load(self,data):
+        self.reshape       = data[      "reshape"]
+        self.data_aug      = data[     "data_aug"]
+        self.input_size    = data[   "input_size"]
+        self.Laskey_noise  = data[ "Laskey_noise"]
+        self.input_reshape = data["input_reshape"]
+
+        self.max_steering = data["max_steering"]
+        self.max_speed    = data[   "max_speed"]
+
+    def print(self):
+        print("\t"*2,"Data augmentation:\t",self.data_aug)
+        print("\t"*2,"Laskey noise:\t\t"   ,self.Laskey_noise)
+        print("\t"*2,"Input size:\t\t"     ,self.input_size)
+        if self.reshape:
+            print("\t"*2,"Reshape:\t"      ,self.input_reshape)
+        print("\t"*2,"Maximum steering:\t",self.max_steering)
+        print("\t"*2,"Maximum speed:\t\t" ,self.max_speed)
+        print("")
+
     def save(self):
         return {
             "reshape"      : self.      reshape,
@@ -177,6 +202,19 @@ class _Scheduler_settings(object):
         self.learning_rate_decay_steps  = 15
         self.learning_rate_decay_factor = 0.5
 
+    def load(self,data):
+        self.available = data["available"]
+
+        self.learning_rate_initial      = data["learning_rate_initial"     ]
+        self.learning_rate_decay_steps  = data["learning_rate_decay_steps" ]
+        self.learning_rate_decay_factor = data["learning_rate_decay_factor"]
+
+    def print(self):
+        print("\t"*3,"Learning rate initial:\t\t"   , self.learning_rate_initial)
+        print("\t"*3,"Learning rate decay factor:\t", self.learning_rate_decay_factor)
+        print("\t"*3,"Learning rate decay steps:\t" , self.learning_rate_decay_steps)
+        print("")
+
     def save(self):
         return {
             "learning_rate_decay_factor" : self.learning_rate_decay_factor,
@@ -193,6 +231,18 @@ class _Optimizer_settings(object):
         self.beta_1        = 0.70   #0.9  #0.7 
         self.beta_2        = 0.85   #0.999#0.85
 
+    def load(self,data):
+        self.type          = data[         "type"]
+        self.beta_1        = data[       "beta_1"]
+        self.beta_2        = data[       "beta_2"]
+        self.learning_rate = data["learning_rate"]
+
+    def print(self):
+        print("\t"*3,"Learning rate:\t", self.learning_rate)
+        print("\t"*3,"Beta 1:\t", self.beta_1)
+        print("\t"*3,"Beta 2:\t", self.beta_2)
+        print("")
+
     def save(self):
         return {
             "type"          : self.         type,
@@ -204,12 +254,29 @@ class _Optimizer_settings(object):
 
 class _Loss_settings(object):
     def __init__(self):
-        self.type          = "weight"
+        self.type          = "Weight"
         self.lambda_steer  = 0.45
         self.lambda_gas    = 0.45
         self.lambda_brake  = 0.10
         self.lambda_action = 0.95
         self.lambda_speed  = 0.05
+
+    def load(self,data):
+        self.type          = data[         "type"]
+        self.lambda_gas    = data[   "lambda_gas"]
+        self.lambda_steer  = data[ "lambda_steer"]
+        self.lambda_brake  = data[ "lambda_brake"]
+        self.lambda_speed  = data[ "lambda_speed"]
+        self.lambda_action = data["lambda_action"]
+
+    def print(self):
+        if self.type is "Weight":
+            print("\t"*3,"Lambda steer:\t" , self.lambda_steer)
+            print("\t"*3,"Lambda gas:\t"   , self.lambda_gas)
+            print("\t"*3,"Lambda brake:\t" , self.lambda_brake)
+            print("\t"*3,"Lambda action:\t", self.lambda_action)
+            print("\t"*3,"Lambda speed:\t" , self.lambda_speed)
+            print("")
 
     def save(self):
         return {
@@ -234,6 +301,29 @@ class Train_settings(object):
 
         self.dropout = 0.5
 
+    def load(self,data):
+        self.n_epoch      = data[     "n_epoch"]
+        self.dropout      = data[     "dropout"]
+        self.batch_size   = data[  "batch_size"]
+        self.sequence_len = data["sequence_len"]
+
+        self.scheduler.load(data["scheduler"])
+        self.optimizer.load(data["optimizer"])
+        self.loss     .load(data[     "loss"])
+
+    def print(self):
+        print("\t"*2,"Batch size:\t",  self.  batch_size)
+        print("\t"*2,"Sequence len:\t",self.sequence_len)
+        print("\t"*2,"Num. epochs:\t", self.     n_epoch)
+        print("\t"*2,"Dropout:\t", self.dropout)
+        print("")
+        print("\t"*2,self.loss.type+" loss")
+        self.loss.print()
+        print("\t"*2,self.optimizer.type+" optimizer")
+        self.optimizer.print()
+        print("\t"*2,"Scheduler")
+        self.scheduler.print()
+
     def save(self):
         return {
             "dropout"      : self.     dropout,
@@ -251,6 +341,13 @@ class Evaluation_settings(object):
     def __init__(self):
         self.metric = "MAE"
 
+    def load(self,data):
+        self.metric = data["metric"]
+    
+    def print(self):
+        print("\t"*2,"Metric:\t",self.metric)
+        print("")
+
     def save(self):
         return {
             "metric": self.metric
@@ -266,6 +363,32 @@ class Setting(object):
         
         self.model   = "Kim2017" # Basic, Multimodal, Codevilla18, Codevilla19, Kim2017
         self.boolean = BooleanConditions(self.model)
+
+    def load(self,path):
+        with open(path) as json_file:
+            data = json.load(json_file)
+
+            self.model = data["model"]
+            self.preprocessing.load(data["preprocessing"])
+            self.evaluation   .load(data[   "evaluation"])
+            self.general      .load(data[      "general"])
+            self.train        .load(data[        "train"])
+
+    def print(self):
+        print("="*80)
+        print("\n","\t"+self.model+" Model")
+        print("\t"+"-"*64,"\n")
+
+        print("\tGeneral")
+        self.general.print()
+        print("\tPreprocessing")
+        self.preprocessing.print()
+        print("\tTrain")
+        self.train.print()
+        print("\tEvaluation")
+        self.evaluation.print()
+
+        print("="*80)
 
     def save(self,path):
         setting = {
--- main.py
+++ main.py
@@ -3,36 +3,30 @@ import argparse
 import config
 
 from common.utils import str2bool
-from ReinforcementLearning.dqnAgent import DQNAgent
+#from ReinforcementLearning.dqnAgent import DQNAgent
 from ImitationLearning.ImitationModel import ImitationModel
 
 _imitationLearningList = ['Basic','Multimodal','Codevilla18','Codevilla19','Kim2017']
 
 class Main():
-
+    """ Constructor """
     def __init__(self,init,setting):
+        self.model   = None
 
-        self.setting = setting
-        self.init    = init
-
-        self.model = None
-
+        # Define seed
+        init.set_seed()
+        
         if setting.model in _imitationLearningList:
             self.model = ImitationModel(init,setting)
         else:
             raise NameError('ERROR 404: Model no found')
-
         self.model.build()
-
+        
     def train(self):
         self.model.execute()
-    """
-    def test (self):
-        self.model.test()
-
     def play (self):
-        self.model.play()
-    """
+        pass
+    
 
 if __name__ == "__main__":
 
@@ -45,23 +39,27 @@ if __name__ == "__main__":
     parser.add_argument("--savedpath" ,type=str,help="Data for saved data")
     parser.add_argument("--n_epoch"   ,type=int,help="Number of epoch for train")
     parser.add_argument("--batch_size",type=int,help="Batch size for train")
+    parser.add_argument("--model"     ,type=str,help="End-to-End model")
 
     parser.add_argument("--optimizer",type=str     ,help="Optimizer method: Adam, RAdam, Ranger")
     parser.add_argument("--scheduler",type=str2bool,help="Use scheduler (boolean)")
 
-    parser.add_argument("--mode" ,type=str,help="Select execution mode: train,test,play")
-    parser.add_argument("--model",type=str,help="Agents model")
+    parser.add_argument("--mode" ,type=str,help="Select execution mode: train,play")
     args = parser.parse_args()
 
     # Setting  
     init    = config.   Init()
     setting = config.Setting()
-
+    setting.print()
+    
+    # Model
+    if args.model  is not None: setting.model = args.model
+    
     # Path
     if args.trainpath  is not None: setting.general.trainPath = args.trainpath
     if args.validpath  is not None: setting.general.validPath = args.validpath
     if args.savedpath  is not None: setting.general.savedPath = args.savedpath
-
+    
     # Train
     if args.n_epoch    is not None: setting.train.n_epoch    = args.n_epoch
     if args.batch_size is not None: setting.train.batch_size = args.batch_size
@@ -71,14 +69,11 @@ if __name__ == "__main__":
 
     # Main program
     main = Main(init,setting)
-    """
+    
     if   args.mode == "train":
         main.train()
-    elif args.mode == "test":
-        main.test()
     elif args.mode == "play":
         main.play()
     else:
-        print("Valid execution modes: train,test,play")
+        print("Valid execution modes: train,play")
 
-    """
