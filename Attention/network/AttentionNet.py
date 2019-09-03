118424c01ce36d5130f89f0702eda53acca632fa
Victor Flores <suryavf@gmail.com>
2019-09-03 13:00:09 -0500

    Minor update to Attention Net

--- Attention/network/AttentionNet.py
+++ Attention/network/AttentionNet.py
@@ -5,13 +5,6 @@ import torch.nn as nn
 import Attention.network.Decoder as deco
 import Attention.network.Encoder as enco
 
-from config import Config
-from config import Global
-
-# Settings
-_global = Global()
-_config = Config()
-
 
 """ Visual Attention Network
     ------------------------
@@ -36,33 +29,4 @@ class Kim2017Net(nn.Module):
         x = self.encoder(img)
         y = self.decoder(x)
         return y
-    
-    """ Save settings """
-    def saveSettings(self,path):
-        setting = {
-            "model"            : _config.            model,
-            "n_epoch"          : _config.          n_epoch,
-            "batch_size"       : _config.       batch_size,
-            "Optimizer":{
-                "type"         : "adam",
-                "Learning_rate": {
-                    "initial"     : _config.learning_rate_initial,
-                    "decay_steps" : _config.learning_rate_decay_steps,
-                    "decay_factor": _config.learning_rate_decay_factor
-                },
-                "beta_1": _config.adam_beta_1,
-                "beta_2": _config.adam_beta_2
-            },
-            "Loss":{
-                "type": "weighted",
-                "Lambda":{
-                    "steer": _config.lambda_steer,
-                    "gas"  : _config.lambda_gas  ,
-                    "brake": _config.lambda_brake
-                }
-            }
-        }
-        
-        with open(path, "w") as write_file:
-            json.dump(setting, write_file, indent=4)
-            
+
