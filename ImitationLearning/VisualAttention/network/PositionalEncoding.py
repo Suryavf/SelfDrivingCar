f15e522f66067b59b6b0529213d9c9ee5741008e
Victor Flores Benites <suryavf@gmail.com>
2020-04-12 00:04:48 -0500

    Fusion PositionalEncoding.py

--- ImitationLearning/VisualAttention/network/PositionalEncoding.py
+++ ImitationLearning/VisualAttention/network/PositionalEncoding.py
@@ -7,44 +7,15 @@ import numpy as np
 
 """ Basic Sinusoids
     ---------------
+    SOURCE: https://github.com/codertimo/BERT-pytorch/blob/master/bert_pytorch/model/embedding/bert.py 
     Ref: Vaswani, A., Shazeer, N., Parmar, N., Uszkoreit, J., Jones, L., Gomez, 
          A. N., Lukasz Kaiser & Polosukhin, I. (2017). "Attention is all you need". 
          In Advances in neural information processing systems (pp. 5998-6008).
  """
 class Vaswani2017(nn.Module):
     """ Constructor """
-    def __init__(self, cube_size):
-        self.D = cube_size[2]  
-        self.H = cube_size[1]
-        self.W = cube_size[0]
-        self.L = cube_size[0]*cube_size[1] 
-
-        self.PE = self._getValues()
-
-    def _getValues(self,n=10000):
-        PE = torch.zeros([self.W,self.H,self.D])
-        for i in range(self.W):
-            for j in range(self.H):
-                for k in range(self.D):
-                    p = np.power(n,(2*k)/self.D)
-                    if (i+j)%2 == 0:
-                        PE[i,j,k] = np.sin( (i+j)/p )
-                    else:
-                        PE[i,j,k] = np.cos( (i+j)/p )
-        return PE
-
-
-    """ Forward """
-    def forward(self):
-        return self.PE
-
-"""
-    Ref: https://github.com/codertimo/BERT-pytorch/blob/master/bert_pytorch/model/embedding/bert.py
-"""
-class PositionalEmbedding(nn.Module):
     def __init__(self, d_model, max_len=512):
-        super().__init__()
-
+        super(Vaswani2017, self).__init__()
         # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len, d_model).float()
         pe.require_grad = False
