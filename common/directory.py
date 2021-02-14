0128b18dfd29cbe1921d4ef3dbdc959b252b3ac6
Victor <suryavf@gmail.com>
2021-02-14 07:51:01 -0500

    Config update

--- common/directory.py
+++ common/directory.py
@@ -5,11 +5,11 @@ Attention      = ['Atten1','Atten2','Atten3','Atten4','Atten5','Atten6','Atten7'
 SpatialAttention  = ['MHSA','MHRSA']
 CategoryAttention = ['ECAnet']
 Control        = ['SumHiddenFeature','BranchesModule','SeqModule']
-FilesForStudy100 = ['episode_00044','episode_00219','episode_00350','episode_00406','episode_00555','episode_00592','episode_00670','episode_00751',
-                    'episode_01231','episode_01559','episode_01768','episode_01798','episode_02042','episode_04371','episode_05123',
-                    'episode_00085','episode_00304','episode_00472','episode_00591','episode_00708','episode_01001',
+FilesForStudy100 = ['episode_00044','episode_00219','episode_00350','episode_00406','episode_00592','episode_00751',
+                    'episode_01231','episode_01768','episode_01798','episode_02042','episode_04371','episode_05123',
+                    'episode_00085','episode_00304','episode_00591','episode_00708','episode_01001',
                     'episode_01294','episode_01654','episode_01928','episode_03260','episode_04311','episode_05073',
                     'episode_00145','episode_00235','episode_00674','episode_00729','episode_00816','episode_00937',
                     'episode_01234','episode_01860','episode_02797','episode_03368','episode_04314','episode_05118',
-                    'episode_00189','episode_00190','episode_00325','episode_00376','episode_01813','episode_02658','episode_03083',
+                    'episode_00189','episode_00190','episode_00325','episode_01813','episode_02658','episode_03083',
                     'episode_03103','episode_03535','episode_04370','episode_04577','episode_04578','episode_05011']
