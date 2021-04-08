a7d0139bcbdb84770cd45f6ae2680b9d27b7c059
Victor <suryavf@gmail.com>
2021-04-07 23:11:22 -0500

    Update feature study

--- study/feature.py
+++ study/feature.py
@@ -5,7 +5,7 @@ import numpy as np
 from   matplotlib import pyplot as plt
 
 # Figure list
-use_dist = True
+use_sim = True
 
 # Parameters
 path      = '/media/victor/Documentos/'
@@ -20,9 +20,9 @@ def getint(name):
 filesname = glob.glob(os.path.join(path,'*.sy'))
 filesname.sort(key=getint)
 
-# Initialize distribution
-if use_dist:
-    dist = np.zeros(n_task,n_feature,n_distr)
+# Initialize similarity study (direction)
+if use_sim:
+    sim = np.zeros(n_task,n_feature,n_feature)
 
 # Big loop
 for n,f in enumerate(filesname):
@@ -31,4 +31,29 @@ for n,f in enumerate(filesname):
     with h5py.File(f, 'r') as h5_file:
         feature = np.array(h5_file['feature'])
         feature = feature.squeeze()             # [batch,task,depth,n_feat]
-        feature = feature.transpose(1,0,3,2)    # [task,batch,n_feat,depth]
+
+    if use_sim:
+        # Compute direction
+        mod = np.linalg.norm(feature, ord=2, axis=2, keepdims=True)
+        direc = feature/mod   # [batch,task,depth,n_feat]
+        
+        # Compute similarity
+        sim_ = np.matmul(direc.transpose(0,1,3,2),direc) # [batch,task,n_feat,n_feat]
+        sim += sim_.mean(axis=0)  # [task,n_feat,n_feat]
+        
+if use_sim:
+    sim = sim/len(filesname)
+
+    fig, axes = plt.subplots(nrows=1, ncols=3)
+    fig.set_size_inches(45,15)
+    umb = int(n_feature/2)
+    for t in range(n_task):
+        axes[t].matshow(sim, cmap='coolwarm',vmin=-1,vmax=1,
+                        extent=(0,n_feature,0,n_feature))
+        axes[t].axis('off')
+        for i in range(n_feature): axes[t].hlines(i,0,n_feature, lw=0.5,ls=':')
+        axes[t].hlines(umb,0,n_feature)
+        for i in range(n_feature): axes[t].vlines(i,0,n_feature, lw=0.5,ls=':')
+        axes[t].vlines(umb,0,n_feature)
+    plt.savefig('ftSim.svg',quality=900)
+    
